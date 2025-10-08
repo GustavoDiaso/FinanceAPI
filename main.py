9769581f9675255e791d3d67b79d601bcc13f431
TheGustavoDiaso <165075774+GustavoDiaso@users.noreply.github.com>
2025-10-08 10:39:36 -0300

    commit 1 - simples

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify, request
+from waitress import serve
+from datetime import datetime, timezone
+
+"""
+endpoints 
+
+/ -> brings informations about the API 
+
+
+"""
+
+app = Flask(__name__)
+
+@app.route('/')
+def api_basic_information():
+    api_basic_info = {
+        'api_name': 'Currency API',
+        'description': 'A simple API to get real-time and historical currency quotes.',
+        'author': 'Gustavo Henrique de Oliveira Dias',
+        'contact': {
+            'github': 'https://github.com/GustavoDiaso',
+            'linkedin': 'https://github.com/GustavoDiaso',
+        },
+        'server_time_utc': datetime.now(timezone.utc).isoformat(),
+        'getting_started': {
+            'example_endpoints': {
+                
+            }
+        }
+
+    }
+
+
+
+
+app.run(port=5000, host='localhost',debug=True)
+
+# Use waitress to serve you API
+# serve(app, host='localhost', port=8080)
