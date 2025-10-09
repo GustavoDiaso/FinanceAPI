270254d7efe4af2183c63b285deab681154c8af4
TheGustavoDiaso <165075774+GustavoDiaso@users.noreply.github.com>
2025-10-09 14:29:52 -0300

    v1

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from flask import Flask, jsonify, request
 from waitress import serve
 from datetime import datetime, timezone
+import toolbox as tb
 
 """
 endpoints 
 
 / -> brings informations about the API 
+/v1/convert?from=X&to=Y&amount=Z&date=D-> Converts a given amount of one currency to another at a given point in time
+
+
+HTML response status: https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status
 
 
 """
+# External APIs will use
+FRANKFURTER_BASE_URL = 'https://api.frankfurter.dev/v1'
 
 app = Flask(__name__)
 
 @app.route('/')
 def api_basic_information():
     api_basic_info = {
+        'success': True,
         'api_name': 'Currency API',
         'description': 'A simple API to get real-time and historical currency quotes.',
         'author': 'Gustavo Henrique de Oliveira Dias',
@@ -24,13 +32,59 @@ def api_basic_information():
         },
         'server_time_utc': datetime.now(timezone.utc).isoformat(),
         'getting_started': {
-            'example_endpoints': {
-                ""
-            }
+            'example_endpoints': [
+                '/v1/convert?from=USD&to=BRL&amount=1',
+                '/v1/convert?from=USD&to=BRL&amount=1&date=2025-10-12',
+            ]
         }
-
     }
+    return jsonify(api_basic_info), 200
 
+
+@app.route('/v1/convert')
+def convert_currency():
+    """Converts a given amount of one currency to another at a given point in time"""
+    from_currency = request.args.get('from')
+    to_currency = request.args.get('to')
+    amount = request.args.get('amount')
+    date = request.args.get('date')
+
+    # If the date is not passed, use the current date. If the date is passed, verify if it's real and put it in the
+    # correct format YYYY-MM-DD
+    if date is None:
+        date = datetime.now().date().isoformat()
+    else:
+        try:
+            date = tb.get_formatted_date(date)
+        except tb.NonExistentDateError:
+            non_existent_date_error = {
+                'success': False,
+                'error_message': f'The date {date} does not exist'
+            }
+            return jsonify(non_existent_date_error), 400
+
+
+
+    # Verifying if all the necessary URL parameters were passed
+    if all([from_currency, to_currency, amount]):
+
+        # Defining the URL parameters which will be used to send a request to FrankFurter API
+        url_params = {
+            'base': from_currency,
+            'symbols': to_currency,
+            'amount': amount
+        }
+        # Lets consume the FrankFurter API to get real-time information on the conversion from one currency to another
+        response = tb.consume_frankfurter_api(f"/v1/{date}", url_params)
+        return jsonify(response)
+
+
+    else:
+        variables_not_passed_error = {
+            'success': False,
+            'error_message': 'The parameters "to", "from" and "amount" are mandatory'
+        }
+        return jsonify(variables_not_passed_error), 400
 
 
 
---
+++ toolbox.py
@@ -0,0 +1,81 @@
+from zipfile import FastLookup
+
+import requests
+import datetime
+
+# External API will use
+FRANKFURTER_BASE_URL = 'https://api.frankfurter.dev'
+
+def consume_frankfurter_api(endpoint: str, params: dict = None) -> dict| None:
+    # Just making sure the first char in the endpoint str is a /
+    if endpoint[0] != "/":
+        endpoint = "/" + endpoint
+
+    url = f"{FRANKFURTER_BASE_URL}{endpoint}"
+
+    # Consuming the API
+    response = requests.get(url, params, timeout=10)
+
+    # automatically raises an exception if the HTTPS request returned an unsuccessful status code
+    response.raise_for_status()
+
+    return response.json()
+
+    # except requests.HTTPError as http_err:
+    #     # the HTTP status of the response was between 400-600
+    #     print(f"HTTP error occurred: {http_err}")
+    # except requests.ConnectionError as conn_err:
+    #     print(f"Connection error occurred: {conn_err}")
+    # except requests.Timeout as timeout_err:
+    #     # The server didn't respond in less than 10 seconds
+    #     print(f"Timeout error occurred: {timeout_err}")
+    # except requests.RequestException as general_err:
+    #     print(f"An error occured: {general_err}")
+
+# print(consume_frankfurter_api("v1/latest", {'base': 'USD', 'symbols': 'EUR'}))
+
+def get_existing_currencies() -> dict:
+    currencies = {"AUD": "Australian Dollar", "BGN": "Bulgarian Lev", "BRL": "Brazilian Real", "CAD": "Canadian Dollar",
+     "CHF": "Swiss Franc", "CNY": "Chinese Renminbi Yuan", "CZK": "Czech Koruna", "DKK": "Danish Krone", "EUR": "Euro",
+     "GBP": "British Pound", "HKD": "Hong Kong Dollar", "HUF": "Hungarian Forint", "IDR": "Indonesian Rupiah",
+     "ILS": "Israeli New Sheqel", "INR": "Indian Rupee", "ISK": "Icelandic Króna", "JPY": "Japanese Yen",
+     "KRW": "South Korean Won", "MXN": "Mexican Peso", "MYR": "Malaysian Ringgit", "NOK": "Norwegian Krone",
+     "NZD": "New Zealand Dollar", "PHP": "Philippine Peso", "PLN": "Polish Złoty", "RON": "Romanian Leu",
+     "SEK": "Swedish Krona", "SGD": "Singapore Dollar", "THB": "Thai Baht", "TRY": "Turkish Lira",
+     "USD": "United States Dollar", "ZAR": "South African Rand"}
+
+    return currencies
+
+def currency_exists(currency: str) -> bool:
+    "Verifies if the given currency exists"
+    currencies = get_existing_currencies()
+    return True if currency in currencies.keys() else False
+
+def date_is_real(str_date: str) -> bool:
+    date_formats = ["%Y-%m-%d", "%d-%m-%Y"]
+    for date_format in date_formats:
+        try:
+            datetime.datetime.strptime(str_date, date_format)
+            return True
+        except ValueError:
+            continue
+
+    return False
+
+
+class NonExistentDateError(ValueError):
+    """This error object will be raised when the Date passed to our get_formatted_date function does not exist"""
+    pass
+
+def get_formatted_date(str_date: str) -> str:
+    if not date_is_real(str_date):
+        raise NonExistentDateError("The given date does not exist")
+
+    try:
+        formatted_date = datetime.datetime.strptime(str_date, "%Y-%m-%d")
+        return formatted_date.date().isoformat()
+    except ValueError:
+        formatted_date = datetime.datetime.strptime(str_date, "%d-%m-%Y")
+        return formatted_date.date().isoformat()
+
+
